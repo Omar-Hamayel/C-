28c8e0e2ea04e0b62d4e299213c4b462be221ff2
Omar_Amjad_Hamayel <168176835+Omar-Hamayel@users.noreply.github.com>
2025-10-07 16:14:18 +0300

    Add files via upload

---
+++ Task2/Task2/Book.cs
@@ -0,0 +1,24 @@
+﻿namespace Taske2
+{
+    internal class Book
+    {
+        public Book(int id, string title, string authorName)
+        {
+            Id = id;
+            Title = title;
+            AuthorName = authorName;
+            IsAvailable = true;
+        }
+
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string AuthorName { get; set; }
+        public bool IsAvailable { get; set; }
+
+        public void DisplayInfo() {
+            string status = IsAvailable ? "Available" : "Borrowed";
+            Console.WriteLine($"ID: {Id}, Title: {Title}, Author: {AuthorName}, Status: {status}");
+
+        }
+    }
+}
---
+++ Task2/Task2/Person.cs
@@ -0,0 +1,16 @@
+﻿namespace Taske2
+{
+    internal abstract class  Person
+    {
+        protected Person(int id, string name)
+        {
+            Id = id;
+            Name = name;
+        }
+
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        public abstract void DispalyInfo();
+    }
+}
